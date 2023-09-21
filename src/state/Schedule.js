457cb9e79f2c90fdabbf8697a49617334a356575
Brian Toal <btoal@salesforce.com>
2023-09-21 00:46:33 -0700

    Added game table markup

--- src/state/Schedule.js
+++ src/state/Schedule.js
@@ -1,31 +1,43 @@
     class Schedule {
+        #_copyright;
+        #_totalItems;
+        #_totalEvents;
+        #_totalGames;
+        #_totalMatches;
+        #_dates;
+
         // Constructor method is called when a new instance is created
-        constructor(copyright, totalItems, totalEvents, totalGames, totalMatches) {
-            this._copyright = copyright;
-            this._totalItems = totalItems;
-            this._totalEvents = totalEvents;
-            this._totalGames = totalGames;
-            this._totalMatches = totalMatches;
+        constructor(copyright, totalItems, totalEvents, totalGames, totalMatches, dates) {
+            this.#_copyright = copyright;
+            this.#_totalItems = totalItems;
+            this.#_totalEvents = totalEvents;
+            this.#_totalGames = totalGames;
+            this.#_totalMatches = totalMatches;
+            this.#_dates = dates;
         }
 
         get copyright() {
-            return this._copyright;
+            return this.#_copyright;
         }
 
         get totalItems() {
-            return this._totalItems;
+            return this.#_totalItems;
         }
 
         get totalEvents() {
-            return this._totalEvents;
+            return this.#_totalEvents;
         }
 
         get totalGames() {
-            return this._totalGames;
+            return this.#_totalGames;
         }
 
         get totalMatches() {
-            return this._totalMatches;
+            return this.#_totalMatches;
+        }
+
+        get dates() {
+            return this.#_dates;
         }
     }
 
