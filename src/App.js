d49093a7b23b07d5475eb82a295219e1eff6866a
toaler <brian.toal@gmail.com>
2023-09-30 23:08:54 -0700

    Added date picker component.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import CalendarComponent from "./CalendarComponent";
 
 function App() {
+
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          <CalendarComponent/>
+          <CalendarComponent/>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
